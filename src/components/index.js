c7ad6ec1b65a058d94e197da124f1b8d50a3be53
Dylan Barry <dylanbarry@outlook.com>
2022-11-15 16:27:45 -0400

    react-chat-window added

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import Chatwindow from "./Chatwindow";
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <Chatwindow />
     </div>
   );
 };
