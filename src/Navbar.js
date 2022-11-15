ca5044a5e807532f6adc1de0c599b69ed8d21295
Dylan Barry <dylanbarry@outlook.com>
2022-11-15 11:49:47 -0400

    Init Repository

---
+++ src/Navbar.js
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  ListItem,
+  IconButton,
+  ListItemText,
+  Avatar,
+  Divider,
+  List,
+  Typography,
+  Box,
+} from "@material-ui/core";
+import {
+  ArrowBack,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail,
+} from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import avatar from "./avatar.png";
+
+//CSS Styles be here
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 250,
+    background: "#511",
+    height: "30rem",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Box className={classes.menuSliderContainer} component="div">
+        <Avatar className={classes.Avatar} src={avatar} alt="Dylan"></Avatar>
+      </Box>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton>
+              <ArrowBack style={{ color: "coral" }} />
+            </IconButton>
+            <Typography variant="h5" style={{ color: "coral" }}>
+              Dylan Barry
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
